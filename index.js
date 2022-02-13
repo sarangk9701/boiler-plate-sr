9107989c6621bf11650dd53b4f06cd2ad36ad434
sarangk <sarangk@sarangkui-MacBookPro.local>
2022-02-13 17:20:00 +0900

    토큰생성

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require("./models/User");
-
 const config = require('./config/key')
 
 
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -37,6 +38,36 @@ app.post('/register', (req,res) =>{
     })
   })
 
+})
+
+app.post('/login',(req,res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email },(err,user)=> {
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }else{
+      //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+      user.comparePassword(req.body.password, (err,isMatch)=> {
+        if(!isMatch)
+          return res.json({ loginSuccess:false, message:"비밀번호가 틀렸습니다."})
+        
+          //비밀번호까지 맞다면 토큰을 생성하기.
+          user.generateToken((err,user)=>{
+            if(err) return res.status(400).send(err);
+
+            //토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+            res.cookie("x_auth",user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId:user._id })
+          })
+      })
+    }
+  })
+
+  
 })
 
 app.listen(port, () => {
